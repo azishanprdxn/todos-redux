92bfac00ab31caa57b0794192580b362f4916f33
Zishan Ansari <zshan.ansari07@gmail.com>
2020-07-27 16:10:31 +0530

    Added info section

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../containers/Header';
 import MainSection from '../containers/MainSection';
+import Info from './Info';
 
 const App = () => (
   <div>
     <Header />
     <MainSection />
+    <Info />
   </div>
 );
 
---
+++ src/components/Info.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+  render() {
+    return (
+      <div className="info-section">
+        <p>Double-click to edit a todo</p>
+        <p>Inspired by petehunt</p>
+        <p>Part of TodoMVC</p>
+      </div>
+    );
+  }
+}
+
+export default Info;
