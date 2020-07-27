455a51d9d8277b4629a843a3416640baeac0d2b3
Zishan Ansari <zshan.ansari07@gmail.com>
2020-07-27 14:33:54 +0530

    Updates

---
+++ precache-manifest.64c848e17a35abb12b63fa9f0db45e13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59a077092f56dc83c91d990b9a1f74e",
+    "url": "/todos-redux/index.html"
+  },
+  {
+    "revision": "afa53b66bc2a7c881536",
+    "url": "/todos-redux/static/css/main.b6f8d712.chunk.css"
+  },
+  {
+    "revision": "aec27980902e2cf82c45",
+    "url": "/todos-redux/static/js/2.566c128e.chunk.js"
+  },
+  {
+    "revision": "3d66a4ed243bca57a1e68168bbc08249",
+    "url": "/todos-redux/static/js/2.566c128e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa53b66bc2a7c881536",
+    "url": "/todos-redux/static/js/main.675bcf1a.chunk.js"
+  },
+  {
+    "revision": "34ed230ef59a7d4f161d",
+    "url": "/todos-redux/static/js/runtime-main.30bf5a5d.js"
+  }
+]);
